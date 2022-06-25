72ceff40d54ecd4418efc05a7e8070ec23e5248f
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2022-06-25 00:27:27 -0700

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faFacebook, faWhatsapp, faInstagram} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
+import '../css/footer.css';
 
 const Footer = () => (
   <div className="footer">
@@ -13,11 +14,31 @@ const Footer = () => (
          <Link to="/project" className='footer-links'>What we do</Link>
         </div>
       </div>
+
+
+      <h2 className='follow-us title-footer'>Follow Us</h2>
+      
       <div className='social-links'>
-        <h2 className='follow-us title-footer'>Follow Us</h2>
-        <a href="www.youtube.com" className='youtube-icone'>
-            <FontAwesomeIcon icon={faYoutube} size='2x'/> Youtube
+      <div>
+      <a href="www.youtube.com" className='youtube-icon foot-icon'>
+            <FontAwesomeIcon icon={faTwitter} size='2x'/> Twitter
         </a>
+
+        <a href="www.youtube.com" className='facebook-icon foot-icon'>
+            <FontAwesomeIcon icon={faFacebook} size='2x'/> Facebook
+        </a>
+      </div>
+
+        <div>
+        <a href="www.youtube.com" className='whatsupicon foot-icon'>
+            <FontAwesomeIcon icon={faWhatsapp} size='2x'/> Whatsapp
+        </a>
+
+        <a href="www.youtube.com" className='Instagramicon foot-icon'>
+            <FontAwesomeIcon icon={faInstagram} size='2x'/> Instagram
+        </a>
+        </div>
+
       </div>
 
   </div>
